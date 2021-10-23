e93791198ff424f2c9be36168c532c5afb1819f7
Akash Yadav <itsaky01@gmail.com>
2021-10-23 12:33:05 +0530

    Removed Xtend usage

--- src/main/java/com/itsaky/lsp/SemanticHighlightsParams.java
+++ src/main/java/com/itsaky/lsp/SemanticHighlightsParams.java
@@ -1,15 +1,12 @@
 package com.itsaky.lsp;
 
 import org.eclipse.lsp4j.TextDocumentIdentifier;
-import org.eclipse.xtend.lib.Property;
-import org.eclipse.xtext.xbase.lib.Pure;
 
-@SuppressWarnings("all")
 public class SemanticHighlightsParams {
-  @Property
+  
   private TextDocumentIdentifier _textDocument;
   
-  @Pure
+  
   public TextDocumentIdentifier getTextDocument() {
     return this._textDocument;
   }
--- src/main/java/com/itsaky/lsp/SemanticHighlightsParams.xtend
+++
@@ -1,7 +0,0 @@
-package com.itsaky.lsp;
-
-import org.eclipse.lsp4j.TextDocumentIdentifier
-
-class SemanticHighlightsParams {
-	@Property TextDocumentIdentifier textDocument
-}
